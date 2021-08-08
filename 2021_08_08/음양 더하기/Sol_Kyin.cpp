59ae55cd8c7bc6ea597bfea4c8527b0b295954f0
Kyu Jin Lee <kjsl4tw@naver.com>
2021-08-08 23:07:01 +0900

    Update Sol_Kyin.cpp

--- 2021_08_08/음양 더하기/Sol_Kyin.cpp
+++ 2021_08_08/음양 더하기/Sol_Kyin.cpp
@@ -7,3 +7,17 @@
 /*                                                */
 /*   WHEN : 2021-08-08                            */
 /* ********************************************** */
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int ret = 0;
+    int idx = -1;
+    
+    while (++idx < absolutes.size())
+        ret += ((signs[idx] == true) ? 1 : -1) * absolutes[idx];
+    return ret;
+}
