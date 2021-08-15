0ccd742e8d61ca038aecca11a184087ba0386f58
Kyu Jin Lee <kjsl4tw@naver.com>
2021-08-15 17:06:40 +0900

    Update Sol_Kyin.cpp

--- 2021_08_15/K번째수/Sol_Kyin.cpp
+++ 2021_08_15/K번째수/Sol_Kyin.cpp
@@ -7,3 +7,24 @@
 /*                                                */
 /*   WHEN : 2021-08-15                            */
 /* ********************************************** */
+
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> ret;
+    vector<int> tmp;
+    int idx = -1, idx_arr;
+    while (++idx < commands.size()){
+        tmp.clear();
+        idx_arr = commands[idx][0] - 2;
+        while (++idx_arr < commands[idx][1])
+            tmp.push_back(array[idx_arr]);
+        sort(tmp.begin(), tmp.end());
+        ret.push_back(tmp[commands[idx][2] - 1]);
+    }
+
+    return ret;
+}
