ea80fcb19bef49343836b376ecc811c1f9f0db4e
Kyu Jin Lee <kjsl4tw@naver.com>
2021-08-15 17:06:14 +0900

    Update Sol_Kyin.cpp

--- 2021_08_15/내적/Sol_Kyin.cpp
+++ 2021_08_15/내적/Sol_Kyin.cpp
@@ -7,3 +7,18 @@
 /*                                                */
 /*   WHEN : 2021-08-15                            */
 /* ********************************************** */
+
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    int ret = 0;
+    int idx = -1;
+    
+    while (++idx < a.size()) 
+        ret += a[idx] * b[idx];
+    return ret;
+}
