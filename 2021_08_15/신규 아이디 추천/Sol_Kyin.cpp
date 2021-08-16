9f9bad2a375ad5549ce537919becfdafc5e172b7
Kyu Jin Lee <kjsl4tw@naver.com>
2021-08-16 09:32:41 +0900

    Update Sol_Kyin.cpp

--- 2021_08_15/신규 아이디 추천/Sol_Kyin.cpp
+++ 2021_08_15/신규 아이디 추천/Sol_Kyin.cpp
@@ -16,7 +16,7 @@
 using namespace std;
 
 string solution(string new_id) {
-        string ret = "";
+    string ret = "";
     int idx = -1;
     
     transform(new_id.begin(), new_id.end(), new_id.begin(), ::tolower);
@@ -30,18 +30,12 @@ string solution(string new_id) {
             if (ret[ret.size() - 1] == '.' && ret[ret.size() - 2] == '.')
                 ret.pop_back();
     }
-    
+  
     if (ret.front() == '.') ret.erase(ret.begin());
-    
     if (ret.back() == '.')ret.pop_back();
-    
     if(ret.empty()) ret = "a";
-    
     if(ret.size() >= 16)  ret = ret.substr(0, 15);
-    
     if(ret.back() == '.') ret.pop_back();
-
-    while(ret.size() <= 2) ret.push_back(ret.back());
-    
+    while(ret.size() <= 2) ret.push_back(ret.back());  
     return ret;
 }
