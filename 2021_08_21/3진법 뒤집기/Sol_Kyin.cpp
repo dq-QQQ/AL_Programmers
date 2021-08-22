1b578f13fde31e23173a355bd117d5e55f214ee8
Kyu Jin Lee <kjsl4tw@naver.com>
2021-08-22 20:55:37 +0900

    Update Sol_Kyin.cpp

--- 2021_08_21/3진법 뒤집기/Sol_Kyin.cpp
+++ 2021_08_21/3진법 뒤집기/Sol_Kyin.cpp
@@ -7,3 +7,40 @@
 /*                                                */
 /*   WHEN : 2021-08-21                            */
 /* ********************************************** */
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int Reverse_Trit(int n);
+
+int solution(int n) {
+    int ret = 0;
+    
+    ret = Reverse_Trit(n);
+    
+    return ret;
+}
+
+int Reverse_Trit(int n){
+    vector <int> trit;
+    int idx, ret = 0, digit = 1;
+    
+    //making trit
+    while (n >= 1){
+        trit.push_back(n % 3);
+        n /= 3;
+    }
+    
+    idx = trit.size();
+    
+    while (--idx >= 0){
+        ret += trit[idx] * digit;
+        digit *= 3;
+    }
+    
+    return ret;
+}
