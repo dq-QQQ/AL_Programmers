722e24222122aa5e782cd49d607025f961fc42ed
Kyu Jin Lee <kjsl4tw@naver.com>
2021-08-22 20:54:05 +0900

    Update Sol_Kyin.cpp

--- 2021_08_21/예산/Sol_Kyin.cpp
+++ 2021_08_21/예산/Sol_Kyin.cpp
@@ -7,3 +7,25 @@
 /*                                                */
 /*   WHEN : 2021-08-21                            */
 /* ********************************************** */
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int ret = 0, idx = -1;
+    
+    sort (d.begin(), d.end());
+    
+    while (++idx < d.size()){
+        if (budget - d[idx] < 0)
+            break;
+        ret++;
+        budget -= d[idx];
+    }
+    return ret;
+}
